176aae766fd437cfa264c701222b4a5700831377
Gabriel Høst Andersen <94848510+9plus10savage@users.noreply.github.com>
2022-12-07 17:38:18 +0100

    pushing update for problem 1 and problem 3

---
+++ Assignment_5/binaryClassificationModel.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import cross_val_score
+
+def train_naive_bayes_model(dataset, data_format, sign1, sign2):
+    df = pd.read_csv(f'data/{dataset}.{data_format}') # No duplicates found under deduplication, thus omitted.
+
+    # idxs series is used to check whether the horoscope belongs to the given sign(s) as a bool, since its tabular data every horoscope has an associated sign.
+    idxs_sign1 = df['sign'] == sign1
+    idxs_sign2 = df['sign'] == sign2
+    idxs = idxs_sign1 + idxs_sign2
+
+    # uses loc syntax, which takes the argument of our idxs boolean array, in order to extract the data that matches the given signs.
+    corpus = df['horoscope'].loc[idxs].values
+    y = df['sign'].loc[idxs].values
+
+    # CountVectorizer is used to convert text to numerical data, it returns the input as a document-term matrix (numpy.ndarray here), tokenizes each word, and depicts frequency of every tokenized word for each horoscope. 
+    vectorizer = CountVectorizer()
+    X = vectorizer.fit_transform(corpus).toarray()
+
+    # Randomly splits the dataset in to training, which is used to train the model, while the test is used to test the algorithm (on data it hasn't been trained on). Used random_state to set a static seed, normalizing results for easier testing.
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, 
+        test_size=.20,
+        random_state=24
+        )
+
+    # Instanties the model, and then trains it through the calling of the .fit method. Model will be trained to predict the y-label for given X-feature based on word count. Calculates the probability for each sign, and outputs the most likely sign.
+    classifier = MultinomialNB(alpha=1.0, class_prior=None, fit_prior=True)
+    classifier.fit(X_train , y_train)
+
+    # Uses the model which was instantiated and trained early to classify the y-label for each array (horoscope) in the test dataset.  
+    y_pred = classifier.predict(X_test)
+
+    # Compares the results garnered from the model's classification, which was saved to y_pred, with the ground truth in y_test. Returns the True negatives, false positives, false negatives, and true positives in an array (since its a binary classification).
+    cm = confusion_matrix(y_test, y_pred)
+
+    # Visualizing confusion matrix, changing cmap for readability. 
+    vis = ConfusionMatrixDisplay(confusion_matrix=cm)
+    vis.plot(cmap='inferno')
+    plt.savefig('figures/confusion_matrix.png')
+    plt.close()
+
+    # Uses the classifier model to do k-fold cross validation, cv is used to define it as 10 k-folds. 
+    cv_accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+
+    print('The confusion matrix has been visualized and saved in the figures folder.')
+    print(f'Relative Accuracy: {accuracy_score(y_test, y_pred)}')
+    print(f'Accuracy in instances (True positives + True negatives): {accuracy_score(y_test, y_pred, normalize=False)}')
+    print(f'Cross-validation mean score: {cv_accuracies.mean()}')
+    print(f'Cross-validation standard deviation: {cv_accuracies.std()}')
+
+
+def main():
+    train_naive_bayes_model(dataset='horoscopes', data_format='csv', sign1='aries', sign2="capricorn")
+
+if __name__ == '__main__':
+    main()
---
+++ Assignment_5/multiClassificationModel.py
@@ -0,0 +1,86 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB 
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import ConfusionMatrixDisplay
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import cross_val_score
+
+df = pd.read_csv('data/horoscopes.csv')
+
+def get_signs():
+    return list(set(df['sign'].values))
+
+def shorten_strings_in_list(list_to_shorten, character=3):
+    if type(list_to_shorten) is list:
+        new_list = []
+        for item in list_to_shorten:
+            new_list.append(item[0:character])
+        return new_list
+    else:
+        print('Argument was not of appropriate data type (list)')
+        return None # for clarification
+
+def train_naive_bayes_model(signs):
+    try:
+        signs.sort()
+    except AttributeError:
+        print("Please input a list object for the 'signs' argument")
+    except: 
+        print('Undiagnosed error')
+    else:
+        idxs_total = np.empty(shape=len(df['sign'] == signs[0]), dtype='bool')
+        for sign in signs:
+            idxs = df['sign'] == sign
+            idxs_total += idxs.values
+    # Validate datatype as list, try by using list object method. Create empty numpy array with matching shape and dtype, use for loop to add each to idxs_total (boolean array with all true)
+ 
+    corpus = df['horoscope'].loc[idxs_total].values
+    y = df['sign'].loc[idxs_total].values
+
+    vectorizer = CountVectorizer()
+    X = vectorizer.fit_transform(corpus).toarray()
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, 
+        test_size=.20,
+        random_state=24
+    )    
+
+    classifier = MultinomialNB(alpha=1.0, class_prior=None, fit_prior=True)
+    classifier.fit(X_train , y_train)
+
+    y_pred = classifier.predict(X_test)
+
+    cm = confusion_matrix(y_test, y_pred)
+    cm_labels = shorten_strings_in_list(signs, character=3) # shorten for label readability in visualization.
+
+    vis = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=cm_labels)
+    vis.plot(cmap='inferno')
+    plt.savefig('figures/confusion_matrix_multi.png')
+    plt.close()
+
+    cv_accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+    
+    zero_rate_accuracy = 0.084
+    relative_accuracy = accuracy_score(y_test, y_pred)
+    performance_difference = (relative_accuracy - zero_rate_accuracy)
+    performance_difference_percentage = (performance_difference / zero_rate_accuracy) * 100
+
+    print('The confusion matrix has been visualized and saved as confusion_matrix_multi in the figures folder.')
+    print(f'Zero Rate accuracy: {zero_rate_accuracy}')
+    print(f'Relative Accuracy: {"%.3f" % relative_accuracy}')
+    print(f'Performance difference from zero rate: {"%.3f" % performance_difference}')
+    print(f'That correlates to a {"%.1f" % performance_difference_percentage}% change')
+    print(f'Accuracy in instances (True positives + True negatives): {accuracy_score(y_test, y_pred, normalize=False)}')
+    print(f'Cross-validation mean score: {cv_accuracies.mean()}')
+    print(f'Cross-validation standard deviation: {cv_accuracies.std()}') 
+
+def main():
+    train_naive_bayes_model(signs=get_signs())
+
+if __name__ == '__main__':
+    main()
